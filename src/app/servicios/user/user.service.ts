97783a7093cf1162e730bbbc751ede397c89489b
CatalliniGustavo <catallinigustavo@gmail.com>
2022-07-23 21:02:40 -0300

    url user

--- src/app/servicios/user/user.service.ts
+++ src/app/servicios/user/user.service.ts
@@ -10,7 +10,7 @@ import { TokenService } from '../token.service';
 })
 export class UserService {
 
-  URL = 'http://localhost:8080/user/';
+  URL = 'https://ancient-ocean-71608.herokuapp.com/user/';
 
   constructor(private http: HttpClient,private tokenService: TokenService, private datosPorfolio: PorfolioService) { }
 

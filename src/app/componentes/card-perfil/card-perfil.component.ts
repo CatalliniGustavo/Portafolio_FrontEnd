ed671ae5b772b4405934dd8956ad66b99f858c9a
CatalliniGustavo <catallinigustavo@gmail.com>
2022-09-27 13:08:36 -0300

    mejoras habilidades

--- src/app/componentes/card-perfil/card-perfil.component.ts
+++ src/app/componentes/card-perfil/card-perfil.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Hduras } from 'src/app/model/hduras';
 import { user } from 'src/app/model/user.model';
+import { HdurasService } from 'src/app/servicios/hduras.service';
 import { SwitchService } from 'src/app/servicios/switch.service';
 import { TokenService } from 'src/app/servicios/token.service';
 import { UserService } from 'src/app/servicios/user/user.service';
@@ -18,8 +20,10 @@ export class CardPerfilComponent implements OnInit {
   modalSwitch: boolean;
   banner: String = "../../../assets/img/Banner.jpg"
   errMsj!: string;
+  hdurasl: Array<Hduras> = [];
 
   constructor(
+    public hdurasService: HdurasService,
     public userService: UserService,
     private tokenService: TokenService,
     private modalss: SwitchService,
@@ -33,22 +37,32 @@ export class CardPerfilComponent implements OnInit {
     if (this.tokenService.getToken()) {
       this.isLogin = true;
     }
-    this.userService.getUser().subscribe(data => { 
-      this.user = data 
+    this.userService.getUser().subscribe(data => {
+      this.user = data
     });
 
     this.cargarPerfil();
+    this.cargarHduras();
+    
     this.modalss.$modal.subscribe((valor) => {
       this.modalSwitch = valor
       this.cargarPerfil();
+      this.cargarHduras();
     });
+    
+   
   }
 
-
-
+  cargarHduras(): void {
+    this.hdurasService.getHduras().subscribe(data => { this.hdurasl = data });
+    
+  }
+  
+  
+  
   cargarPerfil(): void {
-    this.userService.getUser().subscribe(data => { 
-      this.user = data 
+    this.userService.getUser().subscribe(data => {
+      this.user = data
     })
   }
 

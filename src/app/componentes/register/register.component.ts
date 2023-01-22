37401b3ec55e02f66ff6498c532eec4fdd7fe937
CatalliniGustavo <catallinigustavo@gmail.com>
2023-01-22 12:27:36 -0300

    títulos en registrarse

--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -21,13 +21,14 @@ export class RegisterComponent implements OnInit {
   password!: string;
   roles: string[] = [];
   errMsj!: string;
+  sub = false;
 
   constructor(
     private tokenService: TokenService,
     private authService: AuthService,
     private router: Router,
     private modalService: NgbModal
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     if (this.tokenService.getToken()) {
@@ -38,26 +39,49 @@ export class RegisterComponent implements OnInit {
   }
 
   onRegist(): void {
-    this.nuevoUsuario = new NuevoUsuario(this.nombre, this.email, this.apellido, this.password);
-    this.authService.nuevo(this.nuevoUsuario).subscribe(data => {
-      this.isLogged = true;
-      this.isLogginFail = false;
-      this.tokenService.setToken(data.token);
-      this.tokenService.setUserName(data.email);
-      this.tokenService.setAthorities(data.authorities);
-      this.roles = data.authorities;
-      window.location.reload();
-      this.modalService.dismissAll();
-    }, err => {
-      this.isLogged = false;
-      this.isLogginFail = true;
-      this.errMsj = err.error.mensaje;
-      console.log(this.errMsj);
+    if (this.nombre == undefined || this.nombre.length < 1) {
+      this.errMsj = "Debe ingresar el nombre"
+    } else if (this.apellido == undefined || this.apellido.length < 1) {
+      this.errMsj = "Debe ingresar el apellido"
+    } else if (!this.email.includes("@") || !this.email.includes(".")) {
+      this.errMsj = "Email mal ingrezado"
+    } else if (this.password.length != 8) {
+      this.errMsj = "Tamaño de la contraseña incorrecto"
+    } else {
 
-    })
+      this.nuevoUsuario = new NuevoUsuario(this.nombre, this.email, this.apellido, this.password);
+
+      this.sub = true;
+      this.errMsj = "...espere porfavor, puede tardar..."
+
+      this.authService.nuevo(this.nuevoUsuario).subscribe(data => {
+        this.isLogged = true;
+        this.isLogginFail = false;
+        this.tokenService.setToken(data.token);
+        this.tokenService.setUserName(data.email);
+        this.tokenService.setAthorities(data.authorities);
+        this.roles = data.authorities;
+        window.location.reload();
+        this.modalService.dismissAll();
+      }, err => {
+
+        this.sub = false;
+        this.isLogged = false;
+        this.isLogginFail = true;
+        this.errMsj = err.error.mensaje;
+        console.log(this.errMsj);
+
+        if (err.error.mensaje != undefined) {
+          this.errMsj = err.error.mensaje
+        } else if (this.errMsj == undefined) {
+          this.errMsj = "Error al conectar con el servidor"
+        }
+
+      })
+    }
   }
 
-  cancel(){
+  cancel() {
     this.modalService.dismissAll();
   }
 }
